da7199249e352cf53420449ac7aebb146b9d5487
Rik <rbose@ur.rochester.edu>
2016-01-18 18:20:10 +1100

    yay I can quickly parse ontology thingies

--- strips/jvm/src/test/scala/ontology.scala
+++ strips/jvm/src/test/scala/ontology.scala
@@ -9,10 +9,15 @@ import utest._
 object StripsOntReaderTests extends TestSuite {
   val tests = TestSuite{
     'load {
-      "as slist" - assert(slist(scala.io.Source.fromInputStream(getClass.getResourceAsStream("/test.lisp")).getLines.mkString("\n")).map(_.size) == Some(23))
+      val sl = slist(scala.io.Source.fromInputStream(getClass.getResourceAsStream("/test.lisp")).getLines.mkString("\n"))
+      "as slist" - assert(sl.map(_.size) == Some(23))
       "as file " - assert(supplies.sexpr.parser.parseAll.parse(
         scala.io.Source.fromInputStream(getClass.getResourceAsStream("/test.lisp")).getLines.mkString("\n")
       ).get.value.size == 23)
+
+      'onttype {
+        "convert" - sl.map(_.tail.map(e => (new OntDefineTypeReader(e.asInstanceOf[ast.Lst].value))()))
+      }
     }
   }
 }
--- strips/shared/src/main/scala/read.scala
+++ strips/shared/src/main/scala/read.scala
@@ -9,22 +9,23 @@ abstract class TypedSReader[T](l : Seq[ast.Expr]) {
    */
   def positionals : Int
 
-  def ordered : Seq[ast.Expr] = l.take(positionals)
-  def hashed  : Map[ast.Expr, ast.Expr]
-    = l.drop(positionals).grouped(2).map(a => a(0) -> a(1)).toMap
+  def ordered : Seq[ast.Expr] = l.take(positionals).toList
+  def hashed  : Map[ast.Expr, Any]
+    = l.drop(positionals).grouped(2).map(a => a(0) -> a(1).value).toMap
 
   def apply : Option[T]
 }
 
 abstract class ScopedData(scope : String) {}
 
-case class OntDefineType(name : String, data : Map[String, ast.Expr]) extends ScopedData("ont")
+case class OntDefineType(name : String, data : Map[String, Any]) extends ScopedData("ont")
 
 class OntDefineTypeReader(l : Seq[ast.Expr]) extends TypedSReader[OntDefineType](l) {
   override def positionals : Int = 2
-  override def apply : Option[OntDefineType] = {
+  override def apply() : Option[OntDefineType] = {
+    println(ordered)
     ordered match {
-      case ast.Var("define-type") :: ast.Var(t) :: Nil => {
+      case ast.Var("define-type") :: ast.Var(t) :: rest => {
         Some(OntDefineType(t, hashed.map(a => a._1.asInstanceOf[ast.Key].asVar.value -> a._2)))
       }
       case _ => None
