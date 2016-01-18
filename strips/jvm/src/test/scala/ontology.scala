e765e13119f40b6cf9aa1655dbf63dca8ad1cf09
Rik <rbose@ur.rochester.edu>
2016-01-18 17:45:35 +1100

    made ontology tests a little nicer

--- strips/jvm/src/test/scala/ontology.scala
+++ strips/jvm/src/test/scala/ontology.scala
@@ -9,13 +9,10 @@ import utest._
 object StripsOntReaderTests extends TestSuite {
   val tests = TestSuite{
     'load {
-      "find file" - getClass.getResourceAsStream("/test.lisp")
-      'load {
-        * - slist(scala.io.Source.fromInputStream(getClass.getResourceAsStream("/test.lisp")).getLines.mkString("\n")).map(_.size)
-        * - supplies.sexpr.parser.parseAll.parse(
-          scala.io.Source.fromInputStream(getClass.getResourceAsStream("/test.lisp")).getLines.mkString("\n")
-        ).get.value.size
-      }
+      "as slist" - assert(slist(scala.io.Source.fromInputStream(getClass.getResourceAsStream("/test.lisp")).getLines.mkString("\n")).map(_.size) == Some(23))
+      "as file " - assert(supplies.sexpr.parser.parseAll.parse(
+        scala.io.Source.fromInputStream(getClass.getResourceAsStream("/test.lisp")).getLines.mkString("\n")
+      ).get.value.size == 23)
     }
   }
 }
